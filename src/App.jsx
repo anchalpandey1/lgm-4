72e4c64ffaf0c6f97e666b6bafc663f8c7dc092d
anchalpandey1 <anchalpandey9076@gmail.com>
2022-10-25 19:28:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = ()=>{
+    return(
+        <React.Fragment>
+            <h1>Welcome to india</h1>
+            <button className="btn btn-success">Thapa technical</button>
+        </React.Fragment>
+    );
+};
+export default App;
